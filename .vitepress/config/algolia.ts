78190dbf73be07ef9ac004eee93beadbf4b83263
jonaslgtm <10245089@qq.com>
2025-11-02 09:30:44 +0800

    Update algolia

--- .vitepress/config/algolia.ts
+++ .vitepress/config/algolia.ts
@@ -3,7 +3,7 @@ import type { DefaultTheme } from 'vitepress'
 export const algolia: DefaultTheme.AlgoliaSearchOptions = {
   appId: '5C1C4WHDZM',
   apiKey: 'd8addf5e57f058fd5a81c1b94bf7f3b4',
-  indexName: '~/notes',
+  indexName: 'Jonas',
   placeholder: '搜索',
   translations: {
     button: {
